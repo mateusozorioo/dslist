e4ebb69d0576619211a01176669fd84c0c96e002
Mateus Ozorio <mateuskozorio@gmail.com>
2025-01-25 16:23:28 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -3,25 +3,20 @@ package com.devsuperior.dslist.dto;
 import com.devsuperior.dslist.entities.Game;
 
 public class GameMinDTO {
-	
+
 	private Long id;
 	private String title;
 	private Integer year;
 	private String imgUrl;
 	private String shortDescription;
-
-	public GameMInDTO() {
-	}
-
+	
 	public GameMinDTO(Game entity) {
-		super();
 		id = entity.getId();
 		title = entity.getTitle();
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
-
 	public Long getId() {
 		return id;
 	}
